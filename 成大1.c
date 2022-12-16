175cc6b8c7a87663880cfbe396e0176211ea51d6
justhandsome1127 <107354621+justhandsome1127@users.noreply.github.com>
2022-12-16 10:50:06 +0800

    Add files via upload

---
+++ 成大1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef long long ll;
+
+int main(){
+    int a;
+    double mile;
+    scanf("%lf",&mile);
+    mile = mile*1.6;
+    printf("%.1lf\n",mile);
+
+
+
+    return 0;
+}
