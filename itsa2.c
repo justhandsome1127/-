be185fc2e390c33f5b9a76939b92c9ce73b62e5b
justhandsome1127 <107354621+justhandsome1127@users.noreply.github.com>
2023-01-06 22:36:22 +0800

    Add files via upload

---
+++ itsa2.c
@@ -0,0 +1,30 @@
+//C_MM01
+//https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=6855
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <limits.h>
+
+typedef long long ll;
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define DEBUG 1
+
+int main(){
+    double a,b,c;
+    scanf("%lf %lf %lf",&a,&b,&c);
+    printf("Trapezoid area:%.1lf\n",(a+b)*c/2);
+
+
+
+
+    
+
+
+
+
+
+
+    return 0;
+}
