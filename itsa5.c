4ac1e142901d7d2bc8d1a1690ec620f5496bcf12
justhandsome1127 <107354621+justhandsome1127@users.noreply.github.com>
2023-01-09 05:41:33 +0800

    Add files via upload

---
+++ itsa5.c
@@ -0,0 +1,32 @@
+//基礎3
+//https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?a=15905
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <limits.h>
+
+typedef long long ll;
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define DEBUG 1
+
+int main(){
+    double a,b;
+   
+	while(scanf("%lf %lf",&a,&b)!=EOF){
+		
+		double c=(a*a)+(b*b); 
+		if(c<10000){
+		    printf("inside\n");
+		}
+		else{
+			printf("outside\n");
+		}
+        
+    }
+    
+    
+
+    return 0;
+}
