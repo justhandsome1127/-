a480bc3d2bfb30a6ba69536598dc480c492903e4
justhandsome1127 <107354621+justhandsome1127@users.noreply.github.com>
2023-01-09 05:50:14 +0800

    Add files via upload

---
+++ itsa7.c
@@ -0,0 +1,28 @@
+//C_MM07
+//https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=6873
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <limits.h>
+
+typedef long long ll;
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define DEBUG 1
+
+int main(){
+    ll a;
+   
+	while(scanf("%lld",&a)!=EOF){
+		
+		
+        
+        printf("%lld %lld %lld\n",a,a*a,a*a*a);
+        
+    }
+    
+    
+
+    return 0;
+}
