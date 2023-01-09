288ef6ced2e523cca7c953d7bb0e414eadd823ed
justhandsome1127 <107354621+justhandsome1127@users.noreply.github.com>
2023-01-09 14:36:06 +0800

    Add files via upload

---
+++ itsa26.c
@@ -0,0 +1,51 @@
+//C_AR02
+//https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=1289
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <limits.h>
+
+typedef long long ll;
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define DEBUG 1
+
+
+
+int main(){
+    ll in,a,b,c,check;
+    
+   
+	
+        
+    int arr[10];
+	for(int i=0;i<6;i++){
+		scanf("%d",&arr[i]);
+	}
+	printf("%d",arr[5]);
+	for(int i=4;i>=0;i--){
+		printf(" %d",arr[i]);
+	}
+	printf("\n");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    return 0;
+}
